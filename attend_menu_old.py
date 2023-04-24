5b1b48c0e7dcb1afd3ea9bb9bf12544f55b5162d
Olayode Enoch <olayodeenoch@gmail.com>
2023-04-24 19:25:19 +0100

    Modified attend.py

--- attend_menu_old.py
+++ attend_menu_old.py
@@ -1,20 +1,18 @@
 import os
 from kivy.uix.screenmanager import Screen
 from kivy.properties import ObjectProperty
-from kivy.lang import Builder
 from kivy.storage.jsonstore import JsonStore
 from kivy_garden.zbarcam import ZBarCam
 from pyzbar.pyzbar import ZBarSymbol
 
-Builder.load_file('attend_menu.kv')
-
+# ...
 
 class AttendMenu(Screen):
     date_input = ObjectProperty(None)
     result_label = ObjectProperty(None)
 
     def scan_qr_code(self):
-        # check if date input is empty
+    # check if date input is empty
         if not self.date_input.text:
             self.result_label.text = 'Please enter a date'
             return
@@ -30,45 +28,44 @@ class AttendMenu(Screen):
         json_store = JsonStore(attendance_file)
 
     # request camera and storage permissions
-    #     from android.permissions import request_permissions, Permission
-    #     request_permissions([
-    #     Permission.CAMERA,
-    #     Permission.WRITE_EXTERNAL_STORAGE,
-    #     Permission.READ_EXTERNAL_STORAGE
-    # ])
+        from android.permissions import request_permissions, Permission
+        request_permissions([
+        Permission.CAMERA,
+        Permission.WRITE_EXTERNAL_STORAGE,
+        Permission.READ_EXTERNAL_STORAGE
+    ])
 
     # start the camera and enable QR code detection
         cam = ZBarCam(
             size_hint=(1, 1),
             pos_hint={'center_x': 0.5, 'center_y': 0.5},
             code_types=[ZBarSymbol.QRCODE, ZBarSymbol.EAN13],
-            # symbol_color=(1, 1, 1, 1),
-            # border_color=(1, 0, 0, 1),
-            # border_width=2,
-            # fps=30,
-            # resolution=(640, 480),
-            # play=True,
-            # orientation='landscape',
-        )
+            symbol_color=(1, 1, 1, 1),
+            border_color=(1, 0, 0, 1),
+            border_width=2,
+            fps=30,
+            resolution=(640, 480),
+            play=True,
+            orientation='landscape',
+    )
 
     # wait for QR code detection
         while True:
             symbols = cam.symbols
             if symbols:
-                # get result from detector
+            # get result from detector
                 result = symbols[0].data.decode()
 
             # check if student has already been marked present (this block doesn't run for the first time')
                 if result in json_store:
                     self.result_label.text = 'Student already marked present'
                 else:
-                    # add student to attendance json file (check if student matric no is in registered students json file)
+                # add student to attendance json file (check if student matric no is in registered students json file)
                     if result not in rjson_store:
                         self.result_label.text = 'Student not registered yet'
                     else:
                         json_store.put(result)
-                        self.result_label.text = 'Attendance marked for {}'.format(
-                            result)
+                        self.result_label.text = 'Attendance marked for {}'.format(result)
 
             # stop the camera
                 cam.play = False
